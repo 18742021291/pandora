59480d13c1b5f9645a379a6ae943de0a147ce74b
xixiaobin001 <xixiaobin001@ke.com>
2019-03-13 15:23:57 +0800

    feature:utils

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Qcloud\Cos\Client;
 class IndexController extends Controller
 {
+    public $cosClient;
     //
+    public function init()
+    {
+        $this->cosClient = new Client(array(
+            'region'      => env('COS_REGION'),
+            'schema'      => 'https', //协议头部，默认为http
+            'credentials' => array(
+                'secretId'  => env('COS_SECRET_ID'),
+                'secretKey' => env('COS_KEY'),
+            ),
+        ));
+    }
 }
---
+++ app/Utils/FileTransferUtils.php
@@ -0,0 +1,12 @@
+<?php
+
+use Qcloud\Cos\Client;
+/**
+ * Copyright © 大连理工大学软件学院
+ * File: FileTransferUtils.php
+ * Author: 郗晓斌
+ * Email: wall1002@mail.dlut.edu.cn
+ * Date: 2019/3/13 11:34 AM
+ * Desc:
+ */
+
